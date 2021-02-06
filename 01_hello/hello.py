fbe83ff119464a09b49c118195e1d01422c47ee4
Савва Морозов <pythontheback@gmail.com>
2021-02-06 19:54:39 +0000

    Reorganize code into the main function

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -5,7 +5,12 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Say hello')
 
-parser.add_argument('-n', '--name', metavar='name',
-                    default='World', help='Name to greet')
-args = parser.parse_args()                               
-print('Hello, ' + args.name + '!')
+def main():
+    parser = argparse.ArgumentParser(description='Say hello')
+    parser.add_argument('-n', '--name', metavar='name',
+                        default='World', help='Name to greet')
+    args = parser.parse_args()
+    print('Hello, ' + args.name + '!')
+
+if __name__ == '__main__':
+    main()
