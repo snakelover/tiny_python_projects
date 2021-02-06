7459afe4e8bdc3559b852520cb442cf76b00e3b3
Савва Морозов <pythontheback@gmail.com>
2021-02-06 19:58:07 +0000

    Fix style

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -10,9 +10,11 @@ def get_args():
                         default='World', help='Name to greet')
     return parser.parse_args()
 
+
 def main():
     args = get_args()
     print('Hello, ' + args.name + '!')
+
 
 if __name__ == '__main__':
     main()
