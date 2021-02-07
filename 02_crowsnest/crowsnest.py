05e22d70387098d31f55b84983d82de04b870f07
Savva Morozov <pythontheback@gmail.com>
2021-02-07 20:57:46 +0300

    Fix typos

--- 02_crowsnest/crowsnest.py
+++ 02_crowsnest/crowsnest.py
@@ -10,20 +10,15 @@ def get_args():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
 
-    parser.add_argument("name", metavar="name", help="A word")
+    parser.add_argument("word", metavar="word", help="A word")
 
     return parser.parse_args()
 
 
 def main():
     args = get_args()
-    pos_arg = args.positional
-
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ""))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    word = args.word
+    print(word)
 
 
 if __name__ == "__main__":
