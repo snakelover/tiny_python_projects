d762ff1b4cf7d664fefc0b415aabe810ca4a8176
Savva Morozov <pythontheback@gmail.com>
2021-02-13 13:47:20 +0300

    Add side option, fix quotes

--- 02_crowsnest/crowsnest.py
+++ 02_crowsnest/crowsnest.py
@@ -8,22 +8,24 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description="Crow's Nest -- choose the correct article",
+        description='Crow\'s Nest -- choose the correct article',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
 
-    parser.add_argument("word", metavar="word", help="A word")
+    parser.add_argument('word', metavar='word', help='A word')
+    parser.add_argument('--side', metavar='side', default='larboard', help='A side of the ship')
 
     return parser.parse_args()
 
 
 def main():
     args = get_args()
+    side = args.side
     word = args.word
     article = 'an' if word[0].lower() in 'aeiou' else 'a'
     if word[0].isupper():
         article = article[0].upper() + article[1:]
-    print(f'Ahoy, Captain, {article} {word} off the larboard bow!')
+    print(f'Ahoy, Captain, {article} {word} off the {side} bow!')
 
 
 if __name__ == "__main__":
